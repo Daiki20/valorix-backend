1eac1d16ecb8a8c7ac11b10ecf3ad26f6b354547
Daiki20 <andrey.pishev2020@yandex.ru>
2026-05-17 16:03:44 +0300

    use DB_PATH env for persistent volume

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3')
 const path = require('path')
 
-const db = new Database(path.join(__dirname, 'valorix.db'))
+const db = new Database(path.join(process.env.DB_PATH || __dirname, 'valorix.db'))
 
 db.pragma('journal_mode = WAL')
 db.pragma('foreign_keys = ON')
